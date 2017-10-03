a083365789fc83447f0dae341fef51f1e1ba7373
andrey.romanenkov <amicus765@gmail.com>
2017-10-03 23:13:35 +0300

    File8

--- src/index.js
+++ src/index.js
@@ -1,51 +1,26 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { render } from 'react-dom'
 
 import './css/style.css';
 
 var my_news = [
   {
-    author: 'Саша Печкин',
-    text: 'В четчерг, четвертого числа...',
-    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+    author: 'Саша',
+    text: 'Новость 1',
+    bigText: 'Большой, очень большой, огромный текст, который надо скрыть'
   },
   {
-    author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!',
-    bigText: 'А евро 42!'
+    author: 'Женя',
+    text: 'Новость 2',
+    bigText: 'Большой, очень большой, огромный текст, который надо скрыть'
   },
   {
     author: 'Гость',
-    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
-    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+    text: 'Новость 3',
+    bigText: 'Большой, очень большой, огромный текст, который надо скрыть'
   }
 ];
-
-var News = React.createClass({
-  render: function() {
-    var data = this.props.data;
-    var newsTemplate;
-
-    if (data.length > 0) {
-      newsTemplate = data.map(function(item, index) {
-        return (
-          <div key={index}>
-                <Article data={item} />
-          </div>
-        )
-      })
-    } else {
-      newsTemplate = <p>К сожалению новостей нет</p>
-    }
-
-    return (
-      <div className='news'>
-        {newsTemplate}
-        <strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
-      </div>
-    );
-  }
-});
 
 var Article = React.createClass({
   propTypes: {
@@ -71,17 +46,123 @@ var Article = React.createClass({
         visible = this.state.visible;
 
     return (
-        <div className='article'>
-            <p className='news__author'>{author}:</p>
-            <p className='news__text'>{text}</p>
-            <a href="#"
-                onClick={this.readmoreClick}
-                className={'news__readmore ' + (visible ? 'none': '')}>
-                    Подробнее
-            </a>
-            <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
-        </div>
-)
+      <div className='article'>
+        <p className='news__author'>{author}:</p>
+        <p className='news__text'>{text}</p>
+        <a href="#"
+          onClick={this.readmoreClick}
+          className={'news__readmore ' + (visible ? 'none': '')}>
+          Подробнее
+        </a>
+        <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+      </div>
+    )
+  }
+});
+
+var News = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+  getInitialState: function() {
+    return {
+      counter: 0
+    }
+  },
+  render: function() {
+    var data = this.props.data;
+    var newsTemplate;
+
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index) {
+        return (
+          <div key={index}>
+            <Article data={item} />
+          </div>
+        )
+      })
+    } else {
+      newsTemplate = <p>К сожалению новостей нет</p>
+    }
+
+    return (
+      <div className='news'>
+        {newsTemplate}
+        <strong
+          className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+      </div>
+    );
+  }
+});
+
+// --- добавили form ---
+var Add = React.createClass({
+  getInitialState: function() { //устанавливаем начальное состояние (state)
+    return {
+      agreeNotChecked: true,
+      authorIsEmpty: true,
+      textIsEmpty: true
+    };
+  },
+  componentDidMount: function() {
+    ReactDOM.findDOMNode(this.refs.author).focus();
+  },
+  onBtnClickHandler: function(e) {
+    e.preventDefault();
+    var author = ReactDOM.findDOMNode(this.refs.author).value;
+    var text = ReactDOM.findDOMNode(this.refs.text).value;
+    alert(author + '\n' + text);
+  },
+  onCheckRuleClick: function(e) {
+    this.setState({agreeNotChecked: !this.state.agreeNotChecked}); //устанавливаем значение в state
+  },
+  onAuthorChange: function(e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({authorIsEmpty: false})
+    } else {
+      this.setState({authorIsEmpty: true})
+    }
+  },
+  onTextChange: function(e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({textIsEmpty: false})
+    } else {
+      this.setState({textIsEmpty: true})
+    }
+  },
+  render: function() {
+    var agreeNotChecked = this.state.agreeNotChecked,
+        authorIsEmpty = this.state.authorIsEmpty,
+        textIsEmpty = this.state.textIsEmpty;
+    return (
+      <form className='add cf'>
+        <input
+          type='text'
+          className='add__author'
+          onChange={this.onAuthorChange}
+          placeholder='Ваше имя'
+          ref='author'
+        />
+        <textarea
+          className='add__text'
+          onChange={this.onTextChange}
+          placeholder='Текст новости'
+          ref='text'
+        ></textarea>
+        <label className='add__checkrule'>
+          <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
+        </label>
+
+        <button
+          className='add__btn'
+          onClick={this.onBtnClickHandler}
+          ref='alert_button'
+          disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
+          >
+          Показать alert
+        </button>
+      </form>
+    );
   }
 });
 
@@ -89,7 +170,8 @@ var App = React.createClass({
   render: function() {
     return (
       <div className='app'>
-       <h3> Новости </h3>
+        <h3>Новости</h3>
+        <Add /> {/* добавили вывод компонента */}
         <News data={my_news} />
       </div>
     );
